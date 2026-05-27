4fbd9f851edbc4925e9fbb3ef0f438a53c0a8b4c
Liam Khuen <liamkhuen@gmail.com>
2026-05-27 11:56:52 -0500

    Week 2, Day 3: Created a javascript module to build an inverted index tree for tracking system logs.

---
+++ week-2/Day_3-4/system-log-indexer.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const sampleData = "./system_configs-dirty.json";
+
+function logIndexBuilder(file){
+    let logIndex = {}
+    // Initiate the variable outside of the try...catch for accessibility.
+    let logData; 
+    
+    try{
+        logData = JSON.parse(fs.readFileSync(file, 'utf-8'));
+    } catch (err){
+        // Setting default value to null as an easy catch if this variable is used later.
+        logData = null;
+        console.error(err);
+    }
+    
+    for (const entry of logData){
+        recordLog(entry, logIndex);
+    }
+
+    console.log(logIndex);
+    
+}
+
+/* ---------- Helper Functions ---------- */
+
+function recordLog(logEntry, indexObj){
+    const env = logEntry.environment ?? "Unknown";
+    const status = logEntry.status ?? "Unknown";
+
+    // Dynamically build logIndex schema via short-circuit allocation
+    indexObj[env] ??= {};
+    indexObj[env][status] ??= [];
+
+   indexObj[env][status].push(logEntry.id ?? "Unknown");
+}
+
+/* ---------- Main Function Call ---------- */
+logIndexBuilder(sampleData);
